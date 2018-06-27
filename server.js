3daf23ef022b29cd49d3567d08916d4bb8e2fa21
Rajasekhar Ghanta <rajasekhar.ghanta@in.ibm.com>
2018-06-27 17:30:35 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials/');
+//set hbs as view engine
+app.set('view engine', 'hbs');
+
+//app.use blocks execute in the order specified in the source code
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to log to the server.log');
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+//built in middleware to serve static files from public folder
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  // res.send('<h1> Hello Express! </h1>');
+  // res.send({
+  //   name: 'rajasekhar',
+  //   likes: [
+  //     'coding',
+  //     'designing'
+  //   ]
+  // });
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    currentYear: new Date().getFullYear(),
+    welcomeText: 'Hello, Welcome to handlebars'
+  });
+});
+
+app.get('/about', (req, res) => {
+  // res.send('About Page');
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorCode: 400,
+    errorMessage: 'bad request'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('server is up on port 3000');
+});
